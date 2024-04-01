cd48345e9ecacfeaac876b979078b09398d6b828
Nader Najjar <nader@Naders-MacBook-Pro.local>
2024-04-01 19:48:29 -0400

    test fail

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -4,29 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayUtilsTest {
 
-    @Test
-    public void testFindLastNull() {
-        // Do not execute fault
-        assertThrows(NullPointerException.class, () -> {ArrayUtils.findLast(null, 3);});
-    }
-
-
-    @Test
-    public void testFindLastEmpty() {
-        // For any input where y appears in the second or later position, there is no error. Also,
-        // if x is empty, there is no error.
-        assertEquals(-1, ArrayUtils.findLast(new int[]{}, 3));
-    }
-
-    @Test
-    public void testFindLastNonExistent() {
-        assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
-    }
-
-    @Test
-    public void testFindLastExistsFirstElement() {
-        assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
-    }
+//    @Test
+//    public void testFindLastNull() {
+//        // Do not execute fault
+//        assertThrows(NullPointerException.class, () -> {ArrayUtils.findLast(null, 3);});
+//    }
+//
+//
+//    @Test
+//    public void testFindLastEmpty() {
+//        // For any input where y appears in the second or later position, there is no error. Also,
+//        // if x is empty, there is no error.
+//        assertEquals(-1, ArrayUtils.findLast(new int[]{}, 3));
+//    }
+//
+//    @Test
+//    public void testFindLastNonExistent() {
+//        assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
+//    }
+//
+//    @Test
+//    public void testFindLastExistsFirstElement() {
+//        assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
+//    }
 
     @Test
     public void testOddOrPosNull() {
